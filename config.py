302b595bbe464ae6f28159bc6c5ed3da3979fc9c
Dhanush R.S <Dhanushrs013@gmail.com>
2025-06-01 23:22:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://DHANUSH:DHANUSH@cluster0.2dppahj.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = environ.get("DB_URI", "mongodb+srv://FILESTORECLONE:FILESTORECLONE@dynamic.lzu6mpy.mongodb.net/?retryWrites=true&w=majority&appName=Dynamic")
 DB_NAME = environ.get("DB_NAME", "DHANUSH")
 
 # Auto Delete Information
